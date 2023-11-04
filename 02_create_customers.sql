b8d4fd4dbe5ece076b8f2a7b884d31d6b62675bc
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2023-11-04 23:55:00 +0700

    feat: implement create table queries

---
+++ 02_create_customers.sql
@@ -0,0 +1,8 @@
+USE shopi;
+
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(255) NOT NULL,
+    contact VARCHAR(255),
+    email VARCHAR(255) UNIQUE
+);
---
+++ 03_create_categories.sql
@@ -0,0 +1,6 @@
+USE online_store;
+
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
